885e005036ce2ca0ed3af0ef199f6dc52857b647
Adarsh Ranjan <122526796+adarshranjan36@users.noreply.github.com>
2024-08-12 09:27:34 +0000

    right

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,13 @@
-console.log("ADARSH")
+const accountId = 12345
+var accountPass = "ada@123"
+let accountEmail = "adarsh143@gmil.com"
+accountCity = "patna"
+let accountState;
+
+//accountId=9876  -> not allowed
+/*don't use var because of issue in block scope and functional scope*/ 
+accountEmail="ada143@hotmail.com"
+accountPass="adrs@143"
+accountCity="punjab"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
